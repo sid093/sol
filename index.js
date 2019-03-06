6b900a90cbd9357bc4d14aad6b723ed08fa66aca
Siddharth <sidsriv93@gmail.com>
2019-03-06 12:09:56 +0530

    Port fix for Heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const auth = require('./lib/auth');
 const redis = require('./lib/redisHelper');
 const express = require('express');
+const _PORT = process.env.PORT || 3000;
 
 // Initialize Environment variables
 require('dotenv').config();
@@ -13,4 +14,4 @@ app.use(express.json());
 app.use(auth.init());
 app.use('/', require('./api/_router')(auth, redis));
 
-app.listen(3000);
+app.listen(_PORT);
