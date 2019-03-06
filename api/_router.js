ba856935d0dfc88a72968a5212f5b4877ebe3905
Siddharth <sidsriv93@gmail.com>
2019-03-06 12:02:19 +0530

    Array wrapper for host

--- api/_router.js
+++ api/_router.js
@@ -5,15 +5,18 @@ module.exports = exports = function(auth, redis) {
   // Lookup Application in Redis
   router.get('/:id', (req, res) => {
     redis.getAppHost(req.params.id)
-      .then(host => res.send(host))
-      .catch(err => res.send(err));
+      .then(host => res.json({
+        app: req.params.id,
+        hosts: host
+      }))
+      .catch(err => res.json({err:err}));
   });
 
   // Create or Update Application in Redis
   router.post('/', auth.protect('basic'), (req, res) => {
     redis.setAppHost(req.body.app, req.body.host)
-      .then(host => res.send(host))
-      .catch(err => res.send(err));
+      .then(reply => res.json({msg:reply}))
+      .catch(err => res.json({err:err}));
   });
 
   return router;
